8f8a327b09bea1db5593d552c4aa0ee5674d99e9
Rodrigo Solano Marques <solanodeveloper@gmail.com>
2018-10-30 22:06:38 -0300

    v0.0.1 - Adicionar InicioFragment no fragment do layout da PrincipalActivity.java

--- app/src/main/java/br/com/rodrigosolanomarques/manpo/PrincipalActivity.java
+++ app/src/main/java/br/com/rodrigosolanomarques/manpo/PrincipalActivity.java
@@ -1,8 +1,13 @@
 package br.com.rodrigosolanomarques.manpo;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toolbar;
+
+import br.com.rodrigosolanomarques.manpo.inicio.InicioFragment;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -14,6 +19,12 @@ public class PrincipalActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setLogo(R.drawable.ic_apple);
         setActionBar(toolbar);
+
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.fragment, new InicioFragment());
+        fragmentTransaction.commit();
 
     }
 }
